0ea96d9a2317f520ae3a37bc4f577a92697221fa
Lucas Brito <britolmbs@hotmail.com>
2024-11-13 10:39:21 -0300

    Criação do User.ts

--- src/entity/User.ts
+++ src/entity/User.ts
@@ -1,3 +1,19 @@
-import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
-
+import { Entity, PrimaryGeneratedColumn, Column, Entity, OneToMany } from "typeorm";
+import { Post } from "./Post";
 //TODO Crie a entidade de User
+export class User{
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column({ length: 100 })
+    firstName: string;
+
+    @Column({ length: 100 })
+    lastName: string;
+
+    @Column({ length: 100, unique: true })
+    email: string;
+
+    @OneToMany(() => Post, post => post.user)
+    posts: Post[];
+}
