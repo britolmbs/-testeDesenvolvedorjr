0735261778057959726c852706f6ed23bb177bd3
Lucas Brito <britolmbs@hotmail.com>
2024-11-13 10:28:24 -0300

    criação do ormconfig.json

--- init.sql
+++ init.sql
@@ -1,5 +1,18 @@
 USE test_db;
 
 --TODO Crie a tabela de user;
-
+CREATE TABLE IF NOT EXISTS user (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    firtsName VARCHAR(100) NOT NULL,
+    lastName VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE
+);
 --TODO Crie a tabela de posts;
+
+CREATE TABLE IF NOT EXISTS post(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(100) NOT NULL,
+    description VARCHAR(100) NOT NULL,
+    userId INT NOT NULL,
+    FOREING KEY (userId) REFERENCES user(id) ON DELETE CASCADE
+);
